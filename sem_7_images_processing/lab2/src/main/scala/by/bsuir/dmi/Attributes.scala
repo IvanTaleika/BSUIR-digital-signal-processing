ef99005d5e3a3be1cd015f7fb2ba1e36f862db76
Ivan Taleika <ivan.taleika@gmail.com>
2019-09-29 20:32:51 +0300

    K-medoids algorithm

---
+++ sem_7_images_processing/lab2/src/main/scala/by/bsuir/dmi/Attributes.scala
@@ -0,0 +1,9 @@
+package by.bsuir.dmi
+import scala.math._
+case class Attributes(area: Double, perimeter: Double, centerMass: Double, elongation: Double) {
+  def module: Double = sqrt(pow(area, 2) + pow(perimeter, 2) + pow(centerMass, 2) + pow(elongation, 2))
+  def -(a: Attributes) =
+    Attributes(area - a.area, perimeter - a.perimeter, centerMass - a.centerMass, elongation - a.elongation)
+  def +(a: Attributes) =
+    Attributes(area + a.area, perimeter + a.perimeter, centerMass + a.centerMass, elongation + a.elongation)
+}
